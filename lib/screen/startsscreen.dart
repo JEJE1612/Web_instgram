f28d474ddd01edebe65a3ff6a5279dacd91ee399
JEJE1612 <jeje20011612@gamil.com>
2024-09-15 22:37:42 +0300

    frist commit in instagram

---
+++ lib/screen/startsscreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Starterscreen extends StatefulWidget {
+  const Starterscreen({super.key});
+
+  @override
+  State<Starterscreen> createState() => _StarterscreenState();
+}
+
+class _StarterscreenState extends State<Starterscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
